50041e747e9617bb345f6eb89f17ad96b002a4ec
Keiti <keitibr@gmail.com>
2021-12-17 20:02:00 -0300

    first commit

---
+++ AppList.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import AppItem from './AppItem';
+
+export default function AppList() {
+  const [items, setItems] = useState([
+    {id: 1, quantidade: 5, descricao: "arroz" }, 
+    {id: 2, quantidade: 1, descricao: "feijão" }, 
+    {id: 3, quantidade: 0.5, descricao: "lentilha" }, 
+    {id: 4, quantidade: 1, descricao: "massa" }, 
+    {id: 5, quantidade: 1, descricao: "katchup" }, 
+    {id: 6, quantidade: 1, descricao: "queijo-ralado" }
+]);
+  return (
+<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width:'90%' }}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>Lista de Compras</Text>
+      <ScrollView contentContainerStyle={styles.itemsContainer}>
+        {items.map((item) => {
+          return (
+            <AppItem
+              key={item.id}
+              id={item.id}
+              item={item.quantidade + "  de " + item.descricao}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 20
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '90%'
+  },
+ itemsContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: 'stretch',
+    backgroundColor: '#fff'
+  },
+});
